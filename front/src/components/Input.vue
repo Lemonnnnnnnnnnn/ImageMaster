dfd21083a1152c1880aa7589b48b2770bcc397b2
soul <104170577@qq.com>
2025-07-07 23:55:11 +0800

    refactor(settings): update UI layout and enhance input handling
    - Removed the base.css file and adjusted main.css to improve styling.
    - Enhanced the Input component to support attribute binding and updated its props for better flexibility.
    - Revamped the Settings view layout with a more structured design, including sections for download directory and proxy settings.
    - Integrated toast notifications for user feedback on configuration changes.

--- front/src/components/Input.vue
+++ front/src/components/Input.vue
@@ -1,25 +1,35 @@
 <template>
-    <div class="relative">
+    <div class="relative" :class="attrs.class">
         <div v-if="help" class="absolute -top-5 text-xs text-neutral-500 mb-1">{{ help }}</div>
-        <input type="text" :value="modelValue" :placeholder="placeholder" @input="handleInput"
+        <input 
+            v-bind="$attrs"
+            :value="modelValue"
+            @input="handleInput"
             class="w-full px-2 py-2 text-white text-sm outline-0 border-b-1 border-neutral-300 focus:border-b-blue-500 border-solid rounded-sm bg-neutral-700 focus:bg-neutral-800" />
     </div>
 </template>
 
 <script setup lang="ts">
-import { defineProps, defineEmits } from "vue";
+import { defineProps, defineEmits, useAttrs } from "vue";
+
+const attrs = useAttrs();
+// 禁用属性继承，因为我们手动处理
+defineOptions({
+    inheritAttrs: false
+});
 
 const props = defineProps({
-    modelValue: { type: String, default: '' },
+    modelValue: { type: [String, FileList, File], default: '' },
     help: { type: String, default: '' },
-    placeholder: { type: String, default: '' },
 });
 
 const emits = defineEmits(['update:modelValue']);   
 
 const handleInput = (e: Event) => {
     const target = e.target as HTMLInputElement;
+    
     emits('update:modelValue', target.value);
+
 }
 </script>
 
--- front/src/views/Setting/index.vue
+++ front/src/views/Setting/index.vue
@@ -1,13 +1,81 @@
 <template>
-    <div>
+    <div class="flex flex-col gap-8 p-8">
+        <div class="flex flex-col gap-4">
+            <div class="text-xl">下载目录</div>
+            <div class="flex gap-4">
+                <Input class="flex-1 cursor-pointer" v-model="downloadDir" placeholder="请选择下载目录"
+                    @click="changeOutputDir" />
+                <!-- <Button @click="changeOutputDir">
+                    <div class="flex items-center gap-2">
+                        <Save :size="16" class="text-white" />
+                        <span>保存目录</span>
+                    </div>
+                </Button> -->
+            </div>
+        </div>
 
+        <div>
+            <div class="text-xl">漫画库</div>
+            <!-- <div class="flex gap-4">
+                <Input class="flex-1 cursor-pointer" v-model="downloadDir" placeholder="请选择漫画库" @click="changeOutputDir" />
+                <Button @click="changeOutputDir">
+                    <div class="flex items-center gap-2">
+                        <Save :size="16" class="text-white" />
+                        <span>保存目录</span>
+                    </div>
+                </Button>
+            </div> -->
+        </div>
+
+        <div class="flex flex-col gap-4">
+            <div class="text-xl">代理设置</div>
+            <div class="flex gap-4">
+                <Input class="flex-1" v-model="proxyUrl" placeholder="请输入代理地址" />
+                <Button @click="saveProxy">
+                    <div class="flex items-center gap-2">
+                        <Save :size="16" class="text-white" />
+                        <span>保存设置</span>
+                    </div>
+                </Button>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { Input, Button } from '@/components';
+import { onMounted, ref } from 'vue';
+import { GetOutputDir, GetProxy, SetProxy } from '../../../wailsjs/go/storage/API';
+import { Save } from 'lucide-vue-next';
+import { toast } from 'vue-sonner';
+import { SetOutputDir } from '../../../wailsjs/go/library/API';
 
+let proxyUrl = ref("")
+let downloadDir = ref("")
+// let mangaDir = ref("")
+
+function saveProxy() {
+    SetProxy(proxyUrl.value).then(() => {
+        toast.success("设置成功！")
+        refreshConfig()
+    })
+}
+
+async function refreshConfig() {
+    proxyUrl.value = await GetProxy();
+    downloadDir.value = await GetOutputDir();
+}
+
+async function changeOutputDir() {
+    SetOutputDir().then(() => {
+        toast.success("设置成功！")
+        refreshConfig()
+    })
+}
+
+onMounted(async () => {
+    refreshConfig()
+})
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
